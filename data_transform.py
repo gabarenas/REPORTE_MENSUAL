e5d3c0248cad19e943696f0e731ca8b2f6a64cd8
Your Name <you@example.com>
2024-12-06 23:00:05 -0500

    comiteando data_transforma de la rama1

--- data_transform.py
+++ data_transform.py
@@ -11,8 +11,21 @@ try:
     despachos = data[data['Motivo'] == 'DESPACHO']
     # Filtrar los despachos con número de serie vacío o nulo
     despachos_filtrados = despachos[despachos['Número de serie'].notna() & (despachos['Número de serie'] != '')]
+    
+     # Asegurarse de que la columna 'Número de artículo' exista
+    if 'Número de artículo' not in despachos_filtrados.columns:
+        raise KeyError("La columna 'Número de artículo' no se encontró en el archivo.")
+    
+    # Crear la nueva columna 'prefijo'
+    despachos_filtrados['prefijo'] = despachos_filtrados['Número de artículo'].astype(str).str[:3]
+    
+    # Crear la nueva columna 'Tipo de servicio'
+    despachos_filtrados['Tipo de servicio'] = ''
 
-    # Exportar a Excel
+    # Definir las condiciones para agrupar los servicios
+    despachos_filtrados.loc[despachos_filtrados['Servicio'].isin(['BASICO ENTEL', 'BASICO SMART']), 'Tipo de servicio'] = 'BASICO'
+    despachos_filtrados.loc[despachos_filtrados['Servicio'].isin(['FLOTA CLARO', 'FLOTA ENTEL GLOBAL']), 'Tipo de servicio'] = 'FLOTA'
+
     archivo_excel = 'data_filtrada.xlsx'
     despachos_filtrados.to_excel(archivo_excel, index=False)
     
